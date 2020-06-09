4370057270997281c14b9233bbeea8ddeffb6f66
PrairieSnpr <ryan.l.miller@outlook.com>
2020-06-08 21:03:59 -0400

    Update readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fill5320",
-    version="0.0.8",
+    version="0.0.9",
     author="PrairieSnpr",
     author_email="midnryanmiller@gmail.com",
     description="A tool to fill ATF form 5320.20",
